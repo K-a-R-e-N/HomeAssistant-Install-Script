e6e4b0a76003325c4c0d8e6b9d35e15220cd0669
FOR PERFECT THOUGHTS <60580344+K-a-R-e-N@users.noreply.github.com>
2020-02-04 04:10:56 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -45,69 +45,13 @@ until grep "Starting Home Assistant" ./nohup.out
   sleep 10
   done
 echo "# # Убываем процесс hass"
-echo -en "\n"
-
-
-
+killall  -w -s 9 -u homeassistant
 echo "# # Получилось!"
 
-#Установка HASS конфигуратора
-cd /home/homeassistant/.homeassistant
-wget https://raw.githubusercontent.com/danielperna84/hass-configurator/master/configurator.py
-#Выходим из под шелла пользователя homeassistant командой 
-exit
-cd /home/homeassistant/.homeassistant
-sudo chmod 755 configurator.py
-cd
-#Cоздадим сервис, который будет запускать HA при перезагрузке системы
-sudo rm -rf /etc/systemd/system/homeassistant@homeassistant.service
-sudo tee -a /etc/systemd/system/homeassistant@homeassistant.service <<_EOF_
-[Unit]
-Description=Home Assistant
-After=network-online.target
-
-[Service]
-Type=simple
-User=%i
-ExecStart=/srv/homeassistant/bin/hass -c "/home/homeassistant/.homeassistant"
-
-[Install]
-WantedBy=multi-user.target
-_EOF_
-#Cоздадим сервис, который будет запускать HASS Configurator при перезагрузке системы
-sudo rm -rf /etc/systemd/system/hass-configurator.service
-sudo tee -a /etc/systemd/system/hass-configurator.service <<_EOF_
-[Unit]
-Description=HASS-Configurator
-After=network.target
-
-[Service]
-Type=simple
-User=homeassistant
-
-WorkingDirectory=/home/homeassistant/.homeassistant
-
-ExecStart=/usr/bin/python3 /home/homeassistant/.homeassistant/configurator.py
-Restart=always
-
-[Install]
-WantedBy=multi-user.target
-_EOF_
-#Активируем наш сервисы и запустим их
-sudo systemctl --system daemon-reload
-sudo systemctl enable homeassistant@homeassistant.service
-sudo systemctl start homeassistant@homeassistant.service
-sudo systemctl enable hass-configurator.service
-sudo systemctl start hass-configurator.service
-#Добавим быстрый доступ на левую панель HA ! Второй раз не вводить!
-sudo tee -a /home/homeassistant/.homeassistant/configuration.yaml <<_EOF_
-
-#HASS-Configurator
-panel_iframe:
-  configurator:
-    title: Configurator
-    icon: mdi:square-edit-outline
-    url: http://192.168.1.33:3218
-_EOF_
-#Готов
-#sudo nano /home/homeassistant/.homeassistant/configuration.yaml
+echo "# # тест 1!"
+ls
+echo "# # тест 2!"
+mkdir ./tests
+cd ./tests
+echo "# # тест 3!"
+htop
