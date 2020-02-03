f380977351c08faba9644e4912b60117d52c74ab
FOR PERFECT THOUGHTS <60580344+K-a-R-e-N@users.noreply.github.com>
2020-02-04 02:48:45 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -37,19 +37,73 @@ pip3 install homeassistant
 
 echo -en "\n"
 echo "# # Обязательно запускаем HA"
-Здесь запускаем hass
+hass
 
-echo "==============================================================="
-echo "          НУЖНА ПОМОЩЬ !!!! ПОМОГИТЕ"
-echo "===============================================================" && sudo tee -a help <<_EOF_
 
-После запуска команды hass , надо подождать минут 10, там че то грузиться для полноценной работы homeassistant. Если это не делать, то придётся все удалить и заново установить с нуля!
 
-ИТАК , через минут 10 когда все уже, вроде, готово, надо нажать на комбинацию клавиш ctrl+c и подождать пока закончится остановка, после чего только продолжить выполнение следующих команд....
 
-Надо решить проблему этой загвоздки....
-может как то в фоновом режиме запустить??
-или вести лог файл, с постоянным монитором, пока там не появиться определённый текс, после чего авто завершение применить?
 
-Очень надеюсь на вашу помощь...
+
+
+
+
+#Установка HASS конфигуратора
+cd /home/homeassistant/.homeassistant
+wget https://raw.githubusercontent.com/danielperna84/hass-configurator/master/configurator.py
+#Выходим из под шелла пользователя homeassistant командой 
+exit
+cd /home/homeassistant/.homeassistant
+sudo chmod 755 configurator.py
+cd
+#Cоздадим сервис, который будет запускать HA при перезагрузке системы
+sudo rm -rf /etc/systemd/system/homeassistant@homeassistant.service
+sudo tee -a /etc/systemd/system/homeassistant@homeassistant.service <<_EOF_
+[Unit]
+Description=Home Assistant
+After=network-online.target
+
+[Service]
+Type=simple
+User=%i
+ExecStart=/srv/homeassistant/bin/hass -c "/home/homeassistant/.homeassistant"
+
+[Install]
+WantedBy=multi-user.target
 _EOF_
+#Cоздадим сервис, который будет запускать HASS Configurator при перезагрузке системы
+sudo rm -rf /etc/systemd/system/hass-configurator.service
+sudo tee -a /etc/systemd/system/hass-configurator.service <<_EOF_
+[Unit]
+Description=HASS-Configurator
+After=network.target
+
+[Service]
+Type=simple
+User=homeassistant
+
+WorkingDirectory=/home/homeassistant/.homeassistant
+
+ExecStart=/usr/bin/python3 /home/homeassistant/.homeassistant/configurator.py
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+_EOF_
+#Активируем наш сервисы и запустим их
+sudo systemctl --system daemon-reload
+sudo systemctl enable homeassistant@homeassistant.service
+sudo systemctl start homeassistant@homeassistant.service
+sudo systemctl enable hass-configurator.service
+sudo systemctl start hass-configurator.service
+#Добавим быстрый доступ на левую панель HA ! Второй раз не вводить!
+sudo tee -a /home/homeassistant/.homeassistant/configuration.yaml <<_EOF_
+
+#HASS-Configurator
+panel_iframe:
+  configurator:
+    title: Configurator
+    icon: mdi:square-edit-outline
+    url: http://192.168.1.33:3218
+_EOF_
+#Готов
+#sudo nano /home/homeassistant/.homeassistant/configuration.yaml
