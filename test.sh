aa621a6f2bc6f82e9512dfe411e645a4b5e7daad
FOR PERFECT THOUGHTS <60580344+K-a-R-e-N@users.noreply.github.com>
2020-02-04 05:11:36 +0900

    Update test.sh

--- test.sh
+++ test.sh
@@ -6,8 +6,8 @@ echo "          Тестовая Установка Home Assistant"
 echo "==============================================================="
 echo -en "\n"
 echo "# # Создаем виртуальную среду для Home Assistant с выше создной учеткой."
-sudo su homeassistant -c "cd /srv/homeassistant"
-
-
-sudo su homeassistant -c "mkdir ./hello"
+sudo su homeassistant -c "cd /srv/homeassistant && python3 -m venv ."
+sudo su homeassistant -c "cd /srv/homeassistant && source bin/activate"
+sudo su homeassistant -c "cd /srv/homeassistant && python3 -m pip install wheel"
+sudo su homeassistant -c "cd /srv/homeassistant && pip3 install homeassistant"
 sudo -u homeassistant -H -s
