f336da3cbc1396941f81db53b3f747d85bca83fa
FOR PERFECT THOUGHTS <60580344+K-a-R-e-N@users.noreply.github.com>
2020-02-04 05:35:31 +0900

    Update test.sh

--- test.sh
+++ test.sh
@@ -5,7 +5,26 @@ echo "==============================================================="
 echo "          Тестовая Установка Home Assistant"
 echo "==============================================================="
 echo -en "\n"
-echo "# # Создаем виртуальную среду для Home Assistant с выше создной учеткой."
-sudo su homeassistant -c "cd /srv/homeassistant ; python3 -m venv . ; source bin/activate ; python3 -m pip install wheel ; pip3 install homeassistant"
-echo "готово"
-sudo -u homeassistant -H -s
+
+
+echo "# # Запускаем hass и ждем завершения"
+
+sudo su homeassistant -c "cd /srv/homeassistant ;nohup hass &"
+cd /srv/homeassistant
+echo "         ждем завершения..."
+until grep "Starting Home Assistant" ./nohup.out
+  do
+  echo "Прошло еще 10 сек"
+  sleep 10
+  done
+echo "# # Убываем процесс hass"
+killall  -w -s 9 -u homeassistant
+echo "# # Получилось!"
+
+echo "# # тест 1!"
+ls
+echo "# # тест 2!"
+mkdir ./tests
+cd ./tests
+echo "# # тест 3!"
+htop
