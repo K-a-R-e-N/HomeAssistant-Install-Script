5f11e3937c45c96d3bc1574654b05667f5fa414f
FOR PERFECT THOUGHTS <60580344+K-a-R-e-N@users.noreply.github.com>
2020-02-04 00:54:19 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,29 +7,29 @@ echo "==============================================================="
 
 echo -en "\n"
 echo "# # Установим зависимости"
-sudo apt-get install python3 python3-dev python3-venv python3-pip libffi-dev libssl-dev -y </dev/null
+sudo apt-get install python3 python3-dev python3-venv python3-pip libffi-dev libssl-dev -y
 
 echo -en "\n"
 echo "# # Добавим аккаунт для Home Assistant под названием homeassistant"
-sudo useradd -rm homeassistant -G dialout,gpio,i2c </dev/null
+sudo useradd -rm homeassistant -G dialout,gpio,i2c
 
 echo -en "\n"
 echo "# # Далее создадим каталог для установки Home Assistant и сделаем владельцем аккаунт homeassistant"
 cd /srv
-sudo mkdir homeassistant </dev/null
-sudo chown homeassistant:homeassistant homeassistant </dev/null
+sudo mkdir homeassistant
+sudo chown homeassistant:homeassistant homeassistant
 
 echo -en "\n"
 echo "# # Создаем виртуальную среду для Home Assistant с выше создной учеткой."
-sudo -u homeassistant -H -s </dev/null
-cd /srv/homeassistant </dev/null
-python3 -m venv . </dev/null
-source bin/activate </dev/null
-python3 -m pip install wheel </dev/null
+sudo -u homeassistant -H -s
+cd /srv/homeassistant
+python3 -m venv .
+source bin/activate
+python3 -m pip install wheel
 
 echo -en "\n"
 echo "# # Ставим сам Home Assistan"
-pip3 install homeassistant </dev/null
+pip3 install homeassistant
 
 echo -en "\n"
 echo "# # Обязательно запускаем HA"
