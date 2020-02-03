6d3a55dc4a7d3de07bd4e0228a7e6a121f56d9fd
FOR PERFECT THOUGHTS <60580344+K-a-R-e-N@users.noreply.github.com>
2020-02-03 23:45:57 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,25 +11,25 @@ sudo apt-get install python3 python3-dev python3-venv python3-pip libffi-dev lib
 
 echo -en "\n"
 echo "# # Добавим аккаунт для Home Assistant под названием homeassistant"
-sudo useradd -rm homeassistant -G dialout,gpio,i2c
+sudo useradd -rm homeassistant -G dialout,gpio,i2c </dev/null
 
 echo -en "\n"
 echo "# # Далее создадим каталог для установки Home Assistant и сделаем владельцем аккаунт homeassistant"
 cd /srv
-sudo mkdir homeassistant
-sudo chown homeassistant:homeassistant homeassistant
+sudo mkdir homeassistant </dev/null
+sudo chown homeassistant:homeassistant homeassistant </dev/null
 
 echo -en "\n"
 echo "# # Создаем виртуальную среду для Home Assistant с выше создной учеткой."
-sudo -u homeassistant -H -s
-cd /srv/homeassistant
-python3 -m venv .
-source bin/activate
-python3 -m pip install wheel
+sudo -u homeassistant -H -s </dev/null
+cd /srv/homeassistant </dev/null
+python3 -m venv . </dev/null
+source bin/activate </dev/null
+python3 -m pip install wheel </dev/null
 
 echo -en "\n"
 echo "# # Ставим сам Home Assistan"
-pip3 install homeassistant
+pip3 install homeassistant </dev/null
 
 echo -en "\n"
 echo "# # Обязательно запускаем HA"
