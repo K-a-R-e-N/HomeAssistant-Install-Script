9530430068ad16245ebc7214b045d3519bd14abc
FOR PERFECT THOUGHTS <60580344+K-a-R-e-N@users.noreply.github.com>
2020-02-04 15:29:01 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,4 +21,4 @@ sudo chown homeassistant:homeassistant homeassistant
 
 echo -en "\n"
 echo "# # Создаем виртуальную среду для Home Assistant с выше создной учеткой."
-sudo su homeassistant -c "cd /srv/homeassistant ; python3 -m venv . ; source bin/activate ; python3 -m pip install wheel ; pip3 install homeassistant"
+sudo su homeassistant -c "cd /srv/homeassistant ; python3 -m venv . ; source bin/activate ; python3 -m pip install wheel ; pip3 install homeassistant ; nohup hass &"
