dc96ae3194ea1fc5f9e87a182cd1b920b77024b4
FOR PERFECT THOUGHTS <60580344+K-a-R-e-N@users.noreply.github.com>
2020-02-04 16:22:50 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -22,17 +22,53 @@ sudo chown homeassistant:homeassistant homeassistant
 echo -en "\n"
 echo "# # Создаем виртуальную среду для Home Assistant с выше создной учеткой."
 sudo rm -rf /srv/homeassistant/nohup.out
-sudo su homeassistant -c "cd /srv/homeassistant ; python3 -m venv . ; source bin/activate ; python3 -m pip install wheel ; pip3 install homeassistant ; nohup hass &"
+sudo su homeassistant -c "cd /srv/homeassistant ; python3 -m venv . ; source bin/activate ; python3 -m pip install wheel ; echo '# # Устновка Home Assistant...' ; pip3 install homeassistant ; nohup hass &"
+
+echo "# # Первый запуск Home Assistant и его настройка..."
 sudo rm -rf /srv/homeassistant/seaech_install.sh
 sudo tee -a /srv/homeassistant/seaech_install.sh <<_EOF_
-echo "         ждем завершения..."
+echo "         это занимает времени, ждем завершения..."
+until grep "Setting up homeassistant" /srv/homeassistant/nohup.out
+  do
+  echo "Прошло еще 10 сек"
+  sleep 10
+  done
+echo "         Настройка homeassistant..."
+until grep "Setting up lovelace" /srv/homeassistant/nohup.out
+  do
+  sleep 10
+  done
+echo "         настройки lovelace подняты..."
+until grep "Setting up frontend" /srv/homeassistant/nohup.out
+  do
+  echo "Прошло еще 10 сек"
+  sleep 10
+  done
+echo "         Настройка внешнего интерфейса..."
 until grep "Starting Home Assistant" /srv/homeassistant/nohup.out
   do
   echo "Прошло еще 10 сек"
   sleep 10
   done
-echo "# # Получилось!"
+echo "         Первый запуск Home Assistant и его настройка завершена..."
 _EOF_
 sudo su homeassistant -c "cd /srv/homeassistant ; bash seaech_install.sh"
-echo "следующий пункт готов"
+echo -en "\n"
+echo "# # Убываем процесс hass"
+killall  -w -s 9 -u homeassistant
+echo -en "\n"
+echo "# # Очищаем хвосты..."
+sudo rm -rf /srv/homeassistant/nohup.out
+sudo rm -rf /srv/homeassistant/seaech_install.sh
 
+
+echo -en "\n"
+echo -en "\n"
+echo -en "\n"
+echo "# # тест 1!"
+ls
+echo "# # тест 2!"
+mkdir ./tests
+cd ./tests
+echo "# # тест 3!"
+htop
