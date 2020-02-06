47e7b5671bf83a6a0067bddaefb6c04e93f9e00f
FOR PERFECT THOUGHTS <60580344+K-a-R-e-N@users.noreply.github.com>
2020-02-06 19:04:51 +0900

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -6,29 +6,29 @@ echo "                     Удаление Home Assistant и его хвост�
 echo "╚═════════════════════════════════════════════════════════════════════════════╝"
 echo -en "\n"
 
-echo -en "\n" ; echo "  # # Убиваем процесс Home Assistant..."
+echo -en "\n" ; echo "  # # Завершение процесса Home Assistant..."
 sudo killall  -w -s 9 -u homeassistant > /dev/null 2>&1
 
-echo -en "\n" ; echo "  # # Деинсталлируем Home Assistant..."
+echo -en "\n" ; echo "  # # Деинсталляция Home Assistant..."
 sudo pip3 uninstall homeassistant > /dev/null 2>&1
 
-echo -en "\n" ; echo "  # # Удаляем пользователя homeassistant..."
+echo -en "\n" ; echo "  # # Удаление пользователя homeassistant..."
 sudo userdel -rf homeassistant > /dev/null 2>&1
 
-
-echo -en "\n" ; echo "  # # Очищаем хвосты сервисов:"
+echo -en "\n" ; echo "  # #  Удаление служб из списока автозагрузки..."
 echo "     - по пути /etc/systemd/system/homeassistant*"
 echo "     - по пути /etc/systemd/system/multi-user.target.wants/homeassistant*"
 sudo rm -rf /etc/systemd/system/homeassistant*
 sudo rm -rf /etc/systemd/system/multi-user.target.wants/homeassistant*
+sudo systemctl --system daemon-reload > /dev/null
 
-echo -en "\n" ; echo "  # # Очищаем хвосты, для возможности последующей нормальной установки:"
+echo -en "\n" ; echo "  # # Удаление хвостов, для возможности последующей нормальной установки:"
 echo "     - по пути /srv/homeassistant"
 echo "     - по пути /run/sudo/ts/homeassistant"
 sudo rm -rf /srv/homeassistant
 sudo rm -rf  /run/sudo/ts/homeassistant
 
-echo -en "\n" ; echo "  # # Очищаем хвосты от плагинов:"
+echo -en "\n" ; echo "  # # Удаление хвостов от плагинов:"
 
 echo -en "\n"
 echo "╔═════════════════════════════════════════════════════════════════════════════╗"
