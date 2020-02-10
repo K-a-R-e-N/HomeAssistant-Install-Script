04c13a2e43aed017cb8fe23f7aed91307d613b42
FOR PERFECT THOUGHTS <60580344+K-a-R-e-N@users.noreply.github.com>
2020-02-11 02:37:13 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -80,7 +80,7 @@ sudo chown homeassistant:homeassistant homeassistant
 exit #Принудительное завершение скрипта!!! Код дальше не работает, а имено 83 строка!! ошибка sh: 1: source: not found
 echo -en "\n" ; echo "  # # Создание виртуальной среды для нового аккаунта..."
 sudo rm -rf /srv/homeassistant/nohup.out ; sudo rm -rf /srv/homeassistant/search_install.sh ; sleep 1
-sudo -u homeassistant -H -s bash -c 'cd /srv/homeassistant && python3 -m venv . && source bin/activate && python3 -m pip install wheel > /dev/null && echo -en "\n" && echo "  # # Установка Home Assistant..." && pip3 install homeassistant > /dev/null && echo "  # # запуск логирования..."  && nohup hass &'
+sudo -u homeassistant -H -s bash -c 'cd /srv/homeassistant && python3 -m venv . && source bin/activate && python3 -m pip install wheel > /dev/null && printf "\n  # # Установка Home Assistant...\n" && pip3 install homeassistant > /dev/null && printf "\n  # # Запуск логирования......\n" && nohup hass &'
 
 echo -en "\n" ; echo -en "\n"
 echo "╔══════════════════════════════════════════════════════════════════╗"
