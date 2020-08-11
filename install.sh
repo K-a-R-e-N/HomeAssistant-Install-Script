2825e8e34ac66e625ee5048ba0ae469ee9e39ce4
FOR PERFECT THOUGHTS <60580344+K-a-R-e-N@users.noreply.github.com>
2020-08-11 13:00:45 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -100,25 +100,25 @@ cd /srv
 sudo mkdir homeassistant
 sudo chown homeassistant:homeassistant homeassistant
 echo -en "\n" ; echo "  # # Создание виртуальной среды для нового аккаунта..."
-        echo "     - Предварительная очистка..."
-sudo rm -rf /srv/homeassistant/nohup.out
-sudo rm -rf /srv/homeassistant/hass-progress.log
-sudo rm -rf /srv/homeassistant/search_install.sh
-sleep 1
-
-#exit #Принудительное завершение скрипта!!! Код дальше не работает, а имено 86 строка!!
 
 #Выполнение через Bash вариант
-sudo -u homeassistant -H -s bash -c 'cd /srv/homeassistant && printf "     - Создание окружения...\n" && python3 -m venv . && printf "     - Активация окружения...\n" && source bin/activate && printf "     - Установка всех зависимостей...\n" && python3 -m pip -q install wheel && printf "\n  # # Установка Home Assistant...\n" && python3 -m pip -q install --no-cache-dir --default-timeout=100 homeassistant'
+sudo -u homeassistant -H -s bash -c 'cd /srv/homeassistant && printf "     - Создание окружения...\n" && python3 -m venv . && printf "     - Активация окружения...\n" && source bin/activate && printf "     - Установка зависимостей для виртуальной среды...\n" && python3 -m pip -q install wheel && printf "\n  # # Установка Home Assistant...\n" && python3 -m pip -q install --no-cache-dir --default-timeout=100 homeassistant'
+
+#Выполнение через sh вариант
+#sudo -u homeassistant -H -s sh -c 'cd /srv/homeassistant && python3 -m venv . && . ./bin/activate && python3 -m pip -q install wheel && printf "\n  # # Установка Home Assistant...\n" && pip -q install homeassistant && printf "\n  # # Запуск логирования......\n" && nohup hass &>/srv/homeassistant/hass-progress.log &'
+#sudo -u homeassistant -H -s sh -c 'cd /srv/homeassistant && python3 -m venv . && . ./bin/activate && printf "\n  # # Запуск логирования......\n" && nohup hass &>/srv/homeassistant/hass-progress.log &'
+
 if [ -d /srv/homeassistant ] && (cd /srv/homeassistant && source ./bin/activate && pip freeze | grep -q homeassistant); then
   echo "     - Home Assistant успешно установлен через PIP..."
 else
   echo -en "\n" ; echo "     - Не удалось установить Home Assistant через PIP!!!"
     GoToMenu
 fi
-#Выполнение через sh вариант
-#sudo -u homeassistant -H -s sh -c 'cd /srv/homeassistant && python3 -m venv . && . ./bin/activate && python3 -m pip -q install wheel && printf "\n  # # Установка Home Assistant...\n" && pip -q install homeassistant && printf "\n  # # Запуск логирования......\n" && nohup hass &>/srv/homeassistant/hass-progress.log &'
-#sudo -u homeassistant -H -s sh -c 'cd /srv/homeassistant && python3 -m venv . && . ./bin/activate && printf "\n  # # Запуск логирования......\n" && nohup hass &>/srv/homeassistant/hass-progress.log &'
+  echo "     - Подготовка к первому запуску..."
+sudo rm -rf /srv/homeassistant/nohup.out
+sudo rm -rf /srv/homeassistant/hass-progress.log
+sudo rm -rf /srv/homeassistant/search_install.sh
+sleep 1
 
 echo -en "\n" ; echo -en "\n"
 echo "╔══════════════════════════════════════════════════════════════════╗"
