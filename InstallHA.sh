00c07b01cc7bbd425195270892e7bb3d8ca0b825
FOR PERFECT THOUGHTS <60580344+K-a-R-e-N@users.noreply.github.com>
2022-10-25 10:28:39 +0900

    Update InstallHA.sh

--- InstallHA.sh
+++ InstallHA.sh
@@ -168,7 +168,7 @@ sudo mkdir /srv/homeassistant
 sudo chown homeassistant:homeassistant /srv/homeassistant
 
 #Создание виртуальной среды для нового аккаунта через Bash вариант
-sudo -u homeassistant -H -s bash -c 'cd /srv/homeassistant && printf "  # # Создание виртуальной среды для нового аккаунта...\n" && python3 -m venv . && printf "     - Активация виртуальной среды...\n" && source bin/activate && printf "     - Установка зависимостей для виртуальной среды...\n" && python3 -m pip -q install wheel && printf "\n  # # Установка Home Assistant...\n" && python3 -m pip -q install --no-cache-dir --default-timeout=100 homeassistant'
+sudo -u homeassistant -H -s bash -c 'cd /srv/homeassistant && printf "\n  # # Создание виртуальной среды для нового аккаунта...\n" && python3 -m venv . && printf "     - Активация виртуальной среды...\n" && source bin/activate && printf "     - Установка зависимостей для виртуальной среды...\n" && python3 -m pip -q install wheel && printf "\n  # # Установка Home Assistant...\n" && python3 -m pip -q install --no-cache-dir --default-timeout=100 homeassistant'
 
 if [ -d /srv/homeassistant ] && (cd /srv/homeassistant && source ./bin/activate && pip freeze | grep -q homeassistant); then
   echo "     ${green}- Home Assistant успешно установлен через PIP...${reset}"
